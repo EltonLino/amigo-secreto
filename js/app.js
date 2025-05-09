008b3bd9d5d6a2a01bb8cc1836bd82a79a842202
Elton Lino <eltonblino@gmail.com>
2025-05-09 19:30:19 -0300

    adicionado proteção contra nomes repetidos

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 let listaDeAmigos = [];
 
-function adicionar(){
-    listaDeAmigos.push(document.getElementById("nome-amigo").value);
-    document.getElementById('lista-amigos').textContent = listaDeAmigos.join(', ');
-    document.getElementById("nome-amigo").value = "";
+function adicionar() {
+    let nome = document.getElementById("nome-amigo").value.trim();
+    if (listaDeAmigos.includes(nome)) {
+        alert(`${nome} já foi adicionado na lista`)
+    } else {
+        listaDeAmigos.push(nome)
+        document.getElementById('lista-amigos').textContent = listaDeAmigos.join(', ');
+        document.getElementById("nome-amigo").value = "";
+    }
 }
 
 function reiniciar() {
@@ -14,5 +19,13 @@ function reiniciar() {
 
 function sortear() {
     let sorteio = listaDeAmigos.sort(() => Math.random() - 0.5);
-    document.getElementById('lista-sorteio').textContent = sorteio.join("->");
+    let listaSorteio = document.getElementById('lista-sorteio');
+    listaSorteio.innerHTML = '';
+    for (let i = 0; i < listaDeAmigos.length; i++) {
+        if (i == listaDeAmigos.length - 1) {
+            listaSorteio.innerHTML = listaSorteio.innerHTML + sorteio[i] + ' -> ' + sorteio[0] + '<br>';
+        } else {
+            listaSorteio.innerHTML = listaSorteio.innerHTML + sorteio[i] + ' -> ' + sorteio[i + 1] + '<br>';
+        }
     }
+}
