4dfa33390d4d098ce289a8e2bd6e8f9de0898f5f
Elton Lino <eltonblino@gmail.com>
2025-05-09 20:13:50 -0300

    proteção contra vazio

--- js/app.js
+++ js/app.js
@@ -2,12 +2,16 @@ let listaDeAmigos = [];
 
 function adicionar() {
     let nome = document.getElementById("nome-amigo").value.trim();
-    if (listaDeAmigos.includes(nome)) {
-        alert(`${nome} já foi adicionado na lista`)
+    if (nome == '') {
+        alert('Escreva o nome do amigo para adicionar na lista');
     } else {
-        listaDeAmigos.push(nome)
-        document.getElementById('lista-amigos').textContent = listaDeAmigos.join(', ');
-        document.getElementById("nome-amigo").value = "";
+        if (listaDeAmigos.includes(nome)) {
+            alert(`${nome} já foi adicionado na lista`)
+        } else {
+            listaDeAmigos.push(nome)
+            document.getElementById('lista-amigos').textContent = listaDeAmigos.join(', ');
+            document.getElementById("nome-amigo").value = "";
+        }
     }
 }
 
@@ -18,14 +22,18 @@ function reiniciar() {
 }
 
 function sortear() {
-    let sorteio = listaDeAmigos.sort(() => Math.random() - 0.5);
-    let listaSorteio = document.getElementById('lista-sorteio');
-    listaSorteio.innerHTML = '';
-    for (let i = 0; i < listaDeAmigos.length; i++) {
-        if (i == listaDeAmigos.length - 1) {
-            listaSorteio.innerHTML = listaSorteio.innerHTML + sorteio[i] + ' -> ' + sorteio[0] + '<br>';
-        } else {
-            listaSorteio.innerHTML = listaSorteio.innerHTML + sorteio[i] + ' -> ' + sorteio[i + 1] + '<br>';
+    if (listaDeAmigos.length > 2) {
+        let sorteio = listaDeAmigos.sort(() => Math.random() - 0.5);
+        let listaSorteio = document.getElementById('lista-sorteio');
+        listaSorteio.innerHTML = '';
+        for (let i = 0; i < listaDeAmigos.length; i++) {
+            if (i == listaDeAmigos.length - 1) {
+                listaSorteio.innerHTML = listaSorteio.innerHTML + sorteio[i] + ' -> ' + sorteio[0] + '<br>';
+            } else {
+                listaSorteio.innerHTML = listaSorteio.innerHTML + sorteio[i] + ' -> ' + sorteio[i + 1] + '<br>';
+            }
         }
+    } else {
+        alert('Para realizar o sorteio é necessário ter pelo menos 3 pessoas na lista')
     }
 }
